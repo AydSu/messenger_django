47a0a4849c2472d49f8f5708b1f88a67c1a50064
ayd <ayd@example.com>
2022-06-21 11:50:27 +0500

    template

--- messenger/messenger_app/urls.py
+++ messenger/messenger_app/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('viewer', view_message)
+    path('viewer', view_all_messages)
 ]
--- messenger/messenger_app/views.py
+++ messenger/messenger_app/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from .models import Message
 
 # Create your views here.
 def index(request: HttpRequest):
     return HttpResponse('Index page')
 
 @csrf_exempt # DELETE THEN
-def view_message(request: HttpRequest):
+def set_message(request: HttpRequest):
     if request.method == 'POST':
         print('+++++++POST')
         print(request.body)
     return HttpResponse('message')
+
+def view_all_messages(request: HttpRequest):
+    messages = Message.objects.order_by('-created_at')
+    context = {
+            "messages": messages,
+            'title': 'Messages'
+        }
+    return render(request, 'messenger_app/view_all_messages.html', context)
+ 
