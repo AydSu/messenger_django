14e1d48e98d1fcc13839608e4ba6aae30a3e59f9
ayd <ayd@example.com>
2022-06-21 10:43:59 +0500

    models

--- messenger/messenger_app/models.py
+++ messenger/messenger_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    user_id = models.PositiveIntegerField()
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    read_flag = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "message" + str(self.id)
