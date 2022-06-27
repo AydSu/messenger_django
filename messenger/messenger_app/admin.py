90972326477b27a4fa1865961f79f16e457c9432
ayd <ayd@example.com>
2022-06-27 17:07:17 +0500

    admin panel

--- messenger/messenger_app/admin.py
+++ messenger/messenger_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Message
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'created_at', 'updated_at', 'read_flag')
+    list_display_links = ('id', 'text')
+
+
+admin.site.register(Message, MessageAdmin)
--- messenger/messenger_app/models.py
+++ messenger/messenger_app/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Message(models.Model):
     user_id = models.PositiveIntegerField()
     text = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-    read_flag = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name='Создано')
+    updated_at = models.DateTimeField(auto_now = True, verbose_name='Отредактировано')
+    read_flag = models.BooleanField(default=False, verbose_name='Прочитано')
 
     def __str__(self):
         return "message" + str(self.id)
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
