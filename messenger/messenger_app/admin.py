d6a8d8381d64b501f63a907f3cfb7e648db4fde0
ayd <ayd@example.com>
2022-06-27 17:31:54 +0500

    foreign table

--- messenger/messenger_app/admin.py
+++ messenger/messenger_app/admin.py
@@ -5,6 +5,8 @@ from .models import Message
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('id', 'text', 'created_at', 'updated_at', 'read_flag')
     list_display_links = ('id', 'text')
+    # list_editable = ('id',)
+    list_filter = ('read_flag',)
 
 
 admin.site.register(Message, MessageAdmin)
--- messenger/messenger_app/models.py
+++ messenger/messenger_app/models.py
@@ -1,11 +1,15 @@
+from statistics import mode
 from django.db import models
 
 class Message(models.Model):
-    user_id = models.PositiveIntegerField()
+    person = models.ForeignKey('Person', on_delete=models.PROTECT, null=True)
+    # user_id = models.PositiveIntegerField()
     text = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add = True, verbose_name='Создано')
     updated_at = models.DateTimeField(auto_now = True, verbose_name='Отредактировано')
     read_flag = models.BooleanField(default=False, verbose_name='Прочитано')
+    group = models.ForeignKey('Group', on_delete=models.PROTECT, null=True)
+    
 
     def __str__(self):
         return "message" + str(self.id)
@@ -13,3 +17,19 @@ class Message(models.Model):
     class Meta:
         verbose_name = 'Сообщение'
         verbose_name_plural = 'Сообщения'
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Название беседы')
+
+    class Meta:
+        verbose_name = 'Беседа'
+        verbose_name_plural = 'Беседы'
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=150, db_index=True, verbose_name='Имя собеседника')
+
+    class Meta:
+        verbose_name = 'Собеседник'
+        verbose_name_plural = 'Собеседники'
